08b35c10386e4a4ea06d83f566d143ad83560496
akshatg5 <akshatgirdhar5@gmail.com>
2024-01-26 13:32:47 +0530

    Added Frontend and Backend edits

--- BackEnd/db.js
+++ BackEnd/db.js
@@ -1,13 +1,19 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb+srv://akshatgirdhar05:8SCcXb8VNSk68Rfc@cluster0.wjdqmfc.mongodb.net/TodoApp")
+const dotenv = require("dotenv").config();
+if (dotenv.error) {
+  throw dotenv.error;
+}
+const dbkey = process.env.DB_KEY;
+console.log("DB_KEY:", dbkey);
+mongoose.connect(dbkey);
 const todoSchema = mongoose.Schema({
-    title: String,
-    description: String,
-    done: Boolean
-})
+  title: String,
+  description: String,
+  done: Boolean,
+});
 
-const todo = mongoose.model('todos', todoSchema);
+const todo = mongoose.model("todos", todoSchema);
 
 module.exports = {
-    todo
-}
+  todo,
+};
