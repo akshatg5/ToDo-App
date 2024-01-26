72484345b4824499285721825b26a8c36a5f44ae
akshatg5 <akshatgirdhar5@gmail.com>
2024-01-26 21:10:23 +0530

    FE edits

--- FrontEnd/src/components/Todos.jsx
+++ FrontEnd/src/components/Todos.jsx
@@ -1,14 +1,41 @@
 // another component that renders all the todos
-import "./Todos.css"
+import React from "react";
+import "./Todos.css";
 
-export function Todos({todos}) {
-    return <div>
-    {todos.map((todo) => {
-        return <div className="todos">
-        <h1>{todo.title}</h1>
-        <h2>{todo.description}</h2>
-        <button>{todo.done == true ? "Completed!!" : "Complete"}</button>
-        </div>
-    })}
+export function Todos({ todos }) {
+  const handleCompleteTodo = (todo) => {
+    fetch(`http://localhost:3000/done`, {
+      method: "PUT",
+      body: JSON.stringify({
+        id: todo.id,
+        completed: true,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then(async (res) => {
+        const json = await res.json();
+        alert("Todo Marked as Complete");
+      })
+      .catch((error) => {
+        console.error("Error completing todo:", error);
+      });
+  };
+
+  return (
+    <div>
+      {todos.map((todo) => {
+        return (
+          <div className="todos" key={todo.id}>
+            <h1>{todo.title}</h1>
+            <h2>{todo.description}</h2>
+            <button onClick={() => handleCompleteTodo(todo)}>
+              {todo.done ? "Completed!!" : "Complete"}
+            </button>
+          </div>
+        );
+      })}
     </div>
+  );
 }
